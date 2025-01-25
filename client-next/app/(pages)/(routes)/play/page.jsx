31d63b7e4039998b81c95eb869bc7893af739555
Nathan Foo <ntfoo27@outlook.com>
2025-01-25 15:59:42 -0600

    Make game code input redirect to start screen

--- client-next/app/(pages)/(routes)/play/page.jsx
+++ client-next/app/(pages)/(routes)/play/page.jsx
@@ -16,6 +16,7 @@ const PlayPage = () => {
   // Game setup
   const [game, setGame] = useState(null);
   const [started, setStarted] = useState(false);
+  const [ended, setEnded] = useState(false);
 
   // Game states
   const [question, setQuestion] = useState(null);
@@ -29,47 +30,11 @@ const PlayPage = () => {
   const [topic, setTopic] = useState("");
   const [rounds, setRounds] = useState("");
 
-  // 
-  useEffect(() => {
-    if (!roomId) return;
-
-    const setGameData = async () => {
-      // const url = `${process.env.NEXT_PUBLIC_SITE_URL}/api/games?gameId=${roomId}`;
-      const testUrl = `${process.env.NEXT_PUBLIC_SITE_URL}/api/games?gameId=8755935`;
-
-      try {
-        const response = await fetch(testUrl);
-
-        console.log(response);
-
-        if (!response.ok) {
-          toast.error('Something went wrong.');
-          throw new Error(`Response status: ${response.status}`);
-        } else {
-          const json = await response.json();
-          const data = json.games[0];
-
-          console.log(data);
-
-          setGame(data);
-
-          setTitle(data.title);
-          setDifficulty(data.difficulty);
-          setTopic(data.topic);
-          setRounds(data.rounds);
-        }
-      } catch (error) {
-        throw new Error(`Something went wrong: ${error}`);
-      }
-    }
-    setGameData();
-  }, [roomId]);
-
   return (
     <div>
       {!game ? (
         // Screen to enter game code
-        <LobbyScreen setRoomId={setRoomId} />
+        <LobbyScreen setRoomId={setRoomId} setGame={setGame} setTitle={setTitle} setDifficulty={setDifficulty} setTopic={setTopic} setRounds={setRounds} />
       ) : (
         <div>
           {!started ? (

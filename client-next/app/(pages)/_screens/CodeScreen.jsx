1c391b59a33d6c28d4cceebdcde3587130d10150
Nathan Foo <ntfoo27@outlook.com>
2025-01-26 03:10:10 -0600

    Feedback screen content

--- client-next/app/(pages)/_screens/CodeScreen.jsx
+++ client-next/app/(pages)/_screens/CodeScreen.jsx
@@ -53,7 +53,7 @@ const CodeScreen = ({ question, setAnswer }) => {
                 )}
               </button>
               <Button className="px-12 py-4 bg-white text-black hover:bg-gray-100 m-5"
-                // onClick={(() => setAnswer('use a two pointer approach, this algorithm has a time complexity of n^4 and a space complexity of n^2'))}
+                onClick={(() => setAnswer('use a two pointer approach, this algorithm has a time complexity of n^4 and a space complexity of n^2'))}
               >
                 Submit
               </Button>

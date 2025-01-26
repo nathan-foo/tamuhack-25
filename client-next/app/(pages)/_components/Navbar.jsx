62002134b90dc53c41ffe9d2d7eb753d3a32116a
Noel Varghese <91435868+NoelVarghese2006@users.noreply.github.com>
2025-01-26 04:32:22 -0600

    rainbow

--- client-next/app/(pages)/_components/Navbar.jsx
+++ client-next/app/(pages)/_components/Navbar.jsx
@@ -4,6 +4,7 @@ import { UserButton, useUser } from '@clerk/nextjs'
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
     const { user } = useUser();
@@ -27,9 +28,13 @@ const Navbar = () => {
         <div className='flex justify-between'>
             <div className='flex items-center justify-start gap-3 w-full'>
                 <Link href='/'>
-                    <div className='text-base ml-3 mr-1 text-white'>
+                    <motion.div className='text-base ml-3 mr-1 text-white'
+                    whileHover={{
+                        color:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
+                        transition: {duration: 1, repeat: Infinity}
+                    }}>
                         <b>RaceCode</b>
-                    </div>
+                    </motion.div>
                 </Link>
             </div>
             <div className='container flex items-center justify-end gap-3 mx-auto'>
